72fa0f8799573c412dd7ac2e60b1dc904f879711
benjamin liu <bliu7@foxmail.com>
2024-02-28 14:17:49 +0800

    init

---
+++ src/test/java/com/expd/geodev/backendpoc/BackendPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expd.geodev.backendpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
